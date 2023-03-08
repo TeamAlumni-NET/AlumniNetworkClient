a66da57feabc1ae771ca3174020195025715e34e
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-03-08 15:17:45 +0200

    feat: localization working

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
-import { Button } from "@mui/material"
+import { Button, MenuItem, Select } from "@mui/material"
 import { strings } from "../utils/localization"
+import PropTypes from 'prop-types';
 
-const NabBar = () => {
+const NabBar = ({language, changeLanguageHandler}) => {
+  /*NabBar.PropTypes = {
+    language: PropTypes.string,
+    changeLanguageHandler: PropTypes.object
+  }
+  console.log(this.props.language)
+  */
   return (
     <div>
+      <Select value={language} onChange={(e) => changeLanguageHandler(e.target.value)}>
+        <MenuItem value={"en"}>En</MenuItem>
+        <MenuItem value={"fi"}>Fi</MenuItem>
+      </Select>
       <NavLink to={"/"}>
         <Button>{strings.navbar.logout}</Button>
       </NavLink>
--- client/src/utils/localization.js
+++ client/src/utils/localization.js
@@ -4,11 +4,34 @@ export const strings = new LocalizedStrings({
   en: {
     navbar:{
       logout: "Log out"
+    },
+    groupList: {
+      title: "Group List",
+      createNewGroup: "Create new group",
+      member: "Member",
+      private: "Private"
+    },
+    topicList: {
+      title: "Topic List",
+      createNewTopic: "Create new topic",
+      member: "Member",
     }
+    
   },
   fi: {
     navbar:{
-      logout: "Kirjaudu ulos"
+      logout: "Kirjaudu ulos",
+    },
+    groupList: {
+      title: "Ryhmä listaus",
+      createNewGroup: "Luo uusi ryhmä",
+      member: "Jäsen",
+      private: "Yksityinen"
+    },
+    topicList: {
+      title: "Ahe listaus",
+      createNewTopic: "Luo uusi aihe",
+      member: "Jäsen",
     }
   }
 })
