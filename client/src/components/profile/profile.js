3eb2e17c8320ee8f4d4dedb1c61c1ce30f1c4e80
KipaTa <kirsi.tainio@gmail.com>
2023-03-08 15:23:22 +0200

    Feat: Keycloak start

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,7 +1,31 @@
+import keycloak from "../../keycloak"
+
 
 const Profile = () => {
+
+console.log(keycloak.login)
   return (
     <div>
+      <h1>Start Page</h1>
+
+      <section className="actions">
+        {!keycloak.authenticated && (
+          <button onClick={() => keycloak.login()}>Login</button>
+        )}
+        {keycloak.authenticated && (
+          <button onClick={() => keycloak.logout()}>Logout</button>
+        )}
+
+      </section>
+
+      {keycloak.token && (
+        <div>
+          <h4>Token</h4>
+          <pre>{keycloak.token}</pre>
+        </div>
+      )}
+
+
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import store from "./store/store"
 import { Provider } from "react-redux"
+import { initialize } from './keycloak'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-)
+
+try {
+  initialize()
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  )
+  
+} catch (error) {
+  root.render(
+    <React.StrictMode>
+      <p>hups</p>
+    </React.StrictMode>
+  )
+}
