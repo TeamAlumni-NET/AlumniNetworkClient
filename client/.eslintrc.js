84cf570008e512353514a5d74cc4f3a1b208ca78
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-03-08 10:05:01 +0200

    First commit

---
+++ client/.eslintrc.js
@@ -0,0 +1,34 @@
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime"
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "react-hooks"
+  ],
+  "rules": {
+    "semi": ["warn", "never"],
+    "prefer-const": "warn",
+    "eqeqeq": "warn",
+    "no-unused-vars": "warn",
+    "indent": ["warn", 2],
+    "no-constant-condition": "warn",
+    "no-empty-function": "warn"
+
+  }
+}
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+const NabBar = () => {
+  return (
+    <div>
+      <NavLink to={"/"}>
+        
+      </NavLink>
+    </div>
+  )
+}
+
+export default NabBar
