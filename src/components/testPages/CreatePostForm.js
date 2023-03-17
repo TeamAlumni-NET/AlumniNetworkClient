55fb85e61ff49a8b7822fa9a1db00511ef261e7c
KipaTa <kirsi.tainio@gmail.com>
2023-03-17 10:02:44 +0200

    feat: create post edits

--- src/components/testPages/CreatePostForm.js
+++ src/components/testPages/CreatePostForm.js
@@ -1,13 +1,59 @@
-import * as React from 'react'
-import { Button, InputLabel, MenuItem, Select, TextField } from "@mui/material"
+import React, { useEffect, useState } from 'react'
+import { Button, InputLabel, MenuItem, Select, TextField, Container, Alert } from "@mui/material"
 import { strings } from "../../utils/localization"
+import keycloak from '../../keycloak'
+import { Box } from '@mui/system'
 
 
 
 
 const CreatePostForm = () => {
-    const [group, setGroup] = React.useState('')
-    const [topic, setTopic] = React.useState('')
+    const [date, setDate] = useState('')
+
+    const [postTitle, setPostTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const [userId, setUserId] = useState('')
+    const [topicId, setTopicId] = useState('')
+    const [groupId, setGroupId] = useState('')
+    const [targetUserId, setTargetUserId] = useState('')
+    const [parentPostId, setParentPostId] = useState('')
+    const [eventId, setEventId] = useState('')
+
+
+    const [groups, setGroups] = useState([])
+    const [topics, setTopics] = useState([])
+    
+    useEffect (() => {
+        const headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + keycloak.token
+            }
+        fetch(process.env.REACT_APP_API_URL + `/api/groups`, {headers})
+            .then(response => response.json())
+            .then(data => setGroups(data))
+            .catch(error => { 
+                Alert.alert('Error', error);
+            });
+    }, [])
+
+    useEffect (() => {
+        const headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + keycloak.token
+            }
+        fetch(process.env.REACT_APP_API_URL + `/api/topics`, {headers})
+            .then(response => response.json())
+            .then(data => setTopics(data))
+            .catch(error => { 
+                Alert.alert('Error', error);
+            });
+    }, [])
+
+    //console.log(groups)
+    //console.log(topics)
+
+ 
 
   const stringList = {
     title: strings.createPostForm.title,
@@ -20,58 +66,67 @@ const CreatePostForm = () => {
   }
 
   return (
-    <>
-        <div style={{margin: 20}}>
-        <div>
+    <Container>
+        
+        <form>
+        <Box>
             <h1>{stringList.title}</h1>
+            <div>
             <InputLabel variant='standard'>{stringList.postTitle}</InputLabel>
-            <TextField 
-            id="outlined-required" 
-            variant="outlined"
-            />
-        </div>
-      
-        <div>
-            <InputLabel variant='standard'>{stringList.group}</InputLabel>
-            <Select
-                value={group}
-                displayEmpty
-            >
-                <MenuItem value="">
-                    <em>{stringList.none}</em>
-                </MenuItem>
-                <MenuItem value={10}>test1</MenuItem>
-                <MenuItem value={20}>test2</MenuItem>
-            </Select>
-        </div>
+              <TextField
+                required
+                id='outlined-required'
+                label={stringList.postTitle}
+                defaultValue=""
+                onChange={e => setPostTitle(e.target.value)}
+              />
+            </div>
 
-        <div>
-            <InputLabel variant='standard'>{stringList.topic}</InputLabel>
-            <Select
-                value={topic}
-                displayEmpty
-            >
+            <div>
+                <InputLabel variant='standard'>{stringList.group}</InputLabel>
+                <Select
+                    value={groups}
+                    displayEmpty
+                >
+                    <MenuItem value="">
+                        <em>{stringList.none}</em>
+                    </MenuItem>
+                </Select>
+            </div>
+
+            <div>
+                <InputLabel variant='standard'>{stringList.topic}</InputLabel>
+                <Select
+                    value={topics}
+                    displayEmpty
+                >
                 <MenuItem value="">
-                    <em>{stringList.none}</em>
-                </MenuItem>
-                <MenuItem value={10}>test1</MenuItem>
-                <MenuItem value={20}>test2</MenuItem>
-            </Select>
-        </div>
-   
-        <div>
+                        <em>{stringList.none}</em>
+                    </MenuItem>
+
+                </Select>
+            </div>
+
+            <div>
             <InputLabel variant='standard'>{stringList.content}</InputLabel>
-            <TextField
-                id="outlined-multiline-static"
+              <TextField
+                required
+                minRows={4}
                 fullWidth
                 multiline
-                rows={4}
-                defaultValue="..."
-            />
-        </div>
-        <Button>{stringList.post}</Button>
-      </div>
-    </>
+                id='outlined-required'
+                label={stringList.content}
+                defaultValue=""
+                onChange={e => setContent(e.target.value)}
+              />
+            </div>
+            <Button>{stringList.post}</Button>
+        </Box>
+
+        </form>
+
+        
+    </Container>
   )
 }
 export default CreatePostForm  
