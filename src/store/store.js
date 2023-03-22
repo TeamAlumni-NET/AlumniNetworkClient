95b466f4763a19f8bed2a1ba99e7374001a69ac4
KipaTa <kirsi.tainio@gmail.com>
2023-03-22 15:05:39 +0200

    Merge: Fixing

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit"
 import authenticationReducer from "../reducers/authenticationSlice"
 import groupListReducer from "../reducers/groupsSlice"
 import topicListReducer from "../reducers/topicsSlice"
-<<<<<<< HEAD
 import postReducer from "../reducers/postSlice"
 
-=======
+
 import postListReducer from "../reducers/postsSlice"
 import eventListReducer from "../reducers/eventsSlice"
 import userReducer from "../reducers/userSlice"
->>>>>>> main
 /**
  * Storage for reducers
  */
@@ -18,12 +16,10 @@ export default configureStore({
     username: authenticationReducer,
     groupList: groupListReducer,
     topicList: topicListReducer,
-<<<<<<< HEAD
-    postForm: postReducer
-=======
+
     postsList: postListReducer,
     eventList: eventListReducer,
-    user: userReducer
->>>>>>> main
+    user: userReducer,
+    postForm: postReducer
   }
 })
