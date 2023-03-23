a03557aa75d98679d94bf2c71a4ad9e4a948ce32
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-03-23 10:26:58 +0200

    updated posts.posts to be posts.childPosts

--- src/Services/post/postService.js
+++ src/Services/post/postService.js
@@ -5,7 +5,7 @@ import keycloak from "../../keycloak"
 export const getPostForTimeline = async () => {
   try {
     const userId = JSON.parse(localStorage.getItem("currentUser")).id
-    const res = await axios.get(`${config.url}/api/posts?userId=${userId}&timeline=true`, {
+    const res = await axios.get(`${config.url}/api/posts?userId=${userId}&target=timeline`, {
       headers: {Authorization: `bearer ${keycloak.token}`}
     })
     return res.data
