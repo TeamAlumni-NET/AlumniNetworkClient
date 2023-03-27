a38b0663750c95f8fcbcfc37b06b1aefad3664b7
jesperi.kuula <jeppe.kuula@gmail.com>
2023-03-27 14:04:10 +0300

    feat: keycloack to production

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,8 +1,14 @@
 import Keycloak from "keycloak-js"
 
-// NB! Leave the / or the relative path will use the Router path
-const keycloak = new Keycloak("/keycloak.json")
+let keycloak 
 
+//eslint-disable-next-line
+if(process.env.NODE_ENV === "production"){
+  keycloak = new Keycloak("/keycloakproduct.json")
+}else{
+// NB! Leave the / or the relative path will use the Router path
+ keycloak = new Keycloak("/keycloak.json")
+}
 /**
  * Initialize Keycloak and silently checking for an existing login.
  * @description Should be called before render() of app.
