49498770feb6ff858c2037ad6439535969b963dc
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-03-30 10:55:26 +0300

    feat: connection from vercel to keycloak

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,14 +1,8 @@
 import Keycloak from "keycloak-js"
 
-let keycloak 
-
-//eslint-disable-next-line
-if(process.env.NODE_ENV === "production"){
-  keycloak = new Keycloak("/keycloakproduct.json")
-}else{
 // NB! Leave the / or the relative path will use the Router path
-  keycloak = new Keycloak("/keycloak.json")
-}
+const keycloak = new Keycloak("/keycloak.json")
+
 /**
  * Initialize Keycloak and silently checking for an existing login.
  * @description Should be called before render() of app.
